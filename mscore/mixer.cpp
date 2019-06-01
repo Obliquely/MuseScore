2b347356963ab170431e400eb92cbcc62e043b41
Matthew Elton <matthew_elton@mac.com>
2019-06-01 12:35:54 +0100

    Working (sort of) mockup. Bit greedy on vertical space.

--- mscore/mixer.cpp
+++ mscore/mixer.cpp
@@ -38,268 +38,7 @@
 #include "mixertrackpart.h"
 #include "mixertrackitem.h"
 
-#include <QStringList>
-
-#include <QList>
-#include <QVariant>
-
-class TreeItem
-{
-public:
-      explicit TreeItem(const QList<QVariant> &data, TreeItem *parentItem = 0);
-      ~TreeItem();
-
-      void appendChild(TreeItem *child);
-
-      TreeItem *child(int row);
-      int childCount() const;
-      int columnCount() const;
-      QVariant data(int column) const;
-      int row() const;
-      TreeItem *parentItem();
-
-private:
-      QList<TreeItem*> m_childItems;
-      QList<QVariant> m_itemData;
-      TreeItem *m_parentItem;
-};
-
-TreeItem::TreeItem(const QList<QVariant> &data, TreeItem *parent)
-{
-      m_parentItem = parent;
-      m_itemData = data;
-}
-
-TreeItem::~TreeItem()
-{
-      qDeleteAll(m_childItems);
-}
-
-void TreeItem::appendChild(TreeItem *item)
-{
-      m_childItems.append(item);
-}
-
-TreeItem *TreeItem::child(int row)
-{
-      return m_childItems.value(row);
-}
-
-int TreeItem::childCount() const
-{
-      return m_childItems.count();
-}
-
-int TreeItem::columnCount() const
-{
-      return m_itemData.count();
-}
-
-QVariant TreeItem::data(int column) const
-{
-      return m_itemData.value(column);
-}
-
-TreeItem *TreeItem::parentItem()
-{
-      return m_parentItem;
-}
-
-int TreeItem::row() const
-{
-      if (m_parentItem)
-            return m_parentItem->m_childItems.indexOf(const_cast<TreeItem*>(this));
-
-      return 0;
-}
-
-
-#include <QAbstractItemModel>
-#include <QModelIndex>
-#include <QVariant>
-
-class TreeItem;
-
-class TreeModel : public QAbstractItemModel
-{
-//      Q_OBJECT
-
-public:
-      explicit TreeModel(const QString &data, QObject *parent = 0);
-      ~TreeModel();
-
-      QVariant data(const QModelIndex &index, int role) const override;
-      Qt::ItemFlags flags(const QModelIndex &index) const override;
-      QVariant headerData(int section, Qt::Orientation orientation,
-                          int role = Qt::DisplayRole) const override;
-      QModelIndex index(int row, int column,
-                        const QModelIndex &parent = QModelIndex()) const override;
-      QModelIndex parent(const QModelIndex &index) const override;
-      int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-      int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-
-private:
-      void setupModelData(const QStringList &lines, TreeItem *parent);
-
-      TreeItem *rootItem;
-};
-
-
-TreeModel::TreeModel(const QString &data, QObject *parent)
-: QAbstractItemModel(parent)
-{
-      QList<QVariant> rootData;
-      rootData << "Title" << "Summary";
-      rootItem = new TreeItem(rootData);
-      setupModelData(data.split(QString("\n")), rootItem);
-}
-
-TreeModel::~TreeModel()
-{
-      delete rootItem;
-}
-
-int TreeModel::columnCount(const QModelIndex &parent) const
-{
-      if (parent.isValid())
-            return static_cast<TreeItem*>(parent.internalPointer())->columnCount();
-      else
-            return rootItem->columnCount();
-}
-
-QVariant TreeModel::data(const QModelIndex &index, int role) const
-{
-      if (!index.isValid())
-            return QVariant();
-
-      if (role != Qt::DisplayRole)
-            return QVariant();
-
-      TreeItem *item = static_cast<TreeItem*>(index.internalPointer());
-
-      return item->data(index.column());
-}
-
-Qt::ItemFlags TreeModel::flags(const QModelIndex &index) const
-{
-      if (!index.isValid())
-            return 0;
-
-      return QAbstractItemModel::flags(index);
-}
-
-QVariant TreeModel::headerData(int section, Qt::Orientation orientation,
-                               int role) const
-{
-      if (orientation == Qt::Horizontal && role == Qt::DisplayRole)
-            return rootItem->data(section);
-
-      return QVariant();
-}
-
-QModelIndex TreeModel::index(int row, int column, const QModelIndex &parent)
-const
-{
-      if (!hasIndex(row, column, parent))
-            return QModelIndex();
-
-      TreeItem *parentItem;
-
-      if (!parent.isValid())
-            parentItem = rootItem;
-      else
-            parentItem = static_cast<TreeItem*>(parent.internalPointer());
-
-      TreeItem *childItem = parentItem->child(row);
-      if (childItem)
-            return createIndex(row, column, childItem);
-      else
-            return QModelIndex();
-}
-
-QModelIndex TreeModel::parent(const QModelIndex &index) const
-{
-      if (!index.isValid())
-            return QModelIndex();
-
-      TreeItem *childItem = static_cast<TreeItem*>(index.internalPointer());
-      TreeItem *parentItem = childItem->parentItem();
-
-      if (parentItem == rootItem)
-            return QModelIndex();
-
-      return createIndex(parentItem->row(), 0, parentItem);
-}
-
-int TreeModel::rowCount(const QModelIndex &parent) const
-{
-      TreeItem *parentItem;
-      if (parent.column() > 0)
-            return 0;
-
-      if (!parent.isValid())
-            parentItem = rootItem;
-      else
-            parentItem = static_cast<TreeItem*>(parent.internalPointer());
-
-      return parentItem->childCount();
-}
-
-void TreeModel::setupModelData(const QStringList &lines, TreeItem *parent)
-{
-      QList<TreeItem*> parents;
-      QList<int> indentations;
-      parents << parent;
-      indentations << 0;
-
-      int number = 0;
-
-      while (number < lines.count()) {
-            int position = 0;
-            while (position < lines[number].length()) {
-                  if (lines[number].at(position) != ' ')
-                        break;
-                  position++;
-            }
-
-            QString lineData = lines[number].mid(position).trimmed();
-
-            if (!lineData.isEmpty()) {
-                  // Read the column data from the rest of the line.
-                  QStringList columnStrings = lineData.split("\t", QString::SkipEmptyParts);
-                  QList<QVariant> columnData;
-                  for (int column = 0; column < columnStrings.count(); ++column)
-                        columnData << columnStrings[column];
-
-                  if (position > indentations.last()) {
-                        // The last child of the current parent is now the new parent
-                        // unless the current parent has no children.
-
-                        if (parents.last()->childCount() > 0) {
-                              parents << parents.last()->child(parents.last()->childCount()-1);
-                              indentations << position;
-                        }
-                  } else {
-                        while (position < indentations.last() && parents.count() > 0) {
-                              parents.pop_back();
-                              indentations.pop_back();
-                        }
-                  }
-
-                  // Append a new item to the current parent's list of children.
-                  parents.last()->appendChild(new TreeItem(columnData, parents.last()));
-            }
-
-            ++number;
-      }
-}
-
 namespace Ms {
-
-
-
-
-
 
 #define _setValue(__x, __y) \
       __x->blockSignals(true); \
@@ -341,7 +80,6 @@ Mixer::Mixer(QWidget* parent)
 
       setWindowFlags(Qt::Tool);
       setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
-      setAllowedAreas(Qt::DockWidgetAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea));
 
       trackAreaLayout = new QHBoxLayout;
       trackAreaLayout->setMargin(0);
@@ -569,6 +307,29 @@ void Mixer::setScore(Score* score)
       partOnlyCheckBox->setEnabled(_activeScore && !_activeScore->isMaster());
       }
 
+
+QWidget* mixerRowWidget(QString name)
+      {
+      //QLabel* labelWidget = new QLabel(name);
+
+      QSlider* slider = new QSlider(Qt::Horizontal);
+      slider->setValue(50);
+
+      QHBoxLayout* sliderLayout = new QHBoxLayout();
+      sliderLayout->setContentsMargins(0, 30, 10, 0);
+      sliderLayout->addWidget(slider);
+
+      QWidget* itemWidget = new QWidget();
+
+      QVBoxLayout *layout = new QVBoxLayout;
+      layout->setAlignment(Qt::AlignTop);
+      //layout->addWidget(labelWidget);
+      layout->addLayout(sliderLayout);
+      layout->setMargin(0);
+      itemWidget->setLayout(layout);
+      return itemWidget;
+      }
+
 //---------------------------------------------------------
 //   updateTracks
 //---------------------------------------------------------
@@ -587,92 +348,53 @@ void Mixer::updateTracks()
                   selPart = _score->parts()[0]->masterPart();
                   selChan = selPart->instrument(Fraction(0,1))->playbackChannel(0, _score->masterScore());
                   }
-
             }
 
-
-      if (trackHolder) {
-            trackAreaLayout->removeWidget(trackHolder);
-            trackHolder->deleteLater();
-            trackHolder = 0;
-            }
-
+      mixerTreeWidget->clear();
       trackList.clear();
-      //mixerDetails->setTrack(0);
-
 
       if (!_score)
             return;
 
-      trackHolder = new QWidget();
-      QHBoxLayout* holderLayout = new QHBoxLayout();
-      holderLayout->setContentsMargins(0, 0, 0, 0);
-      holderLayout->setSpacing(0);
-      trackHolder->setLayout(holderLayout);
-
-      //trackAreaLayout->addWidget(trackHolder);
-
-      // this is where the LIST would be built, I think
-
-      TreeModel model("Something\tSomething description\nNextthing\tNextThing description\n\  Subthing\tSubthing description");
-
-      qDebug()<<"Model rows: "<<model.rowCount();
-      instrumentList->setModel(&model);
+      mixerTreeWidget->setAlternatingRowColors(true);
+      mixerTreeWidget->headerItem()->setHidden(true);
 
       for (Part* localPart : _score->parts()) {
             Part* part = localPart->masterPart();
-            //Add per part tracks
 
-            // THIS would be disclosure triangle
-            bool expanded = expandedParts.contains(part);
-            const InstrumentList* il = part->instruments();
+            const InstrumentList* instrumenList = part->instruments();
 
             Instrument* proxyInstr = nullptr;
             Channel* proxyChan = nullptr;
-            if (!il->empty()) {
-                  il->begin();
-                  proxyInstr = il->begin()->second;
+            if (!instrumenList->empty()) {
+                  instrumenList->begin();
+                  proxyInstr = instrumenList->begin()->second;
                   proxyChan = proxyInstr->playbackChannel(0, _score->masterScore());
                   }
 
-            //model->addAnimal(part->partName(), "Medium");
+            QTreeWidgetItem* item = new QTreeWidgetItem((QTreeWidget*)0, QStringList(part->partName()));
+            mixerTreeWidget->addTopLevelItem(item);
+            mixerTreeWidget->setItemWidget(item, 0, mixerRowWidget(part->partName()));
 
-            //MixerTrackItemPtr mti = std::make_shared<MixerTrackItem>(MixerTrackItem::TrackType::PART, part, proxyInstr, proxyChan);
+            //Add per channel tracks
+            const InstrumentList* il1 = part->instruments();
+            for (auto it = il1->begin(); it != il1->end(); ++it) {
+                  Instrument* instr = it->second;
 
-            //MixerTrackPart* track = new MixerTrackPart(this, mti, expanded);
-            //track->setGroup(this);
-            //trackList.append(track);
-            // holderLayout->addWidget(track);
+                  if (instr->channel().size() <= 1)
+                        continue;
 
-            if (selPart == part &&
-                (selChan == 0 || !expanded)) {
-                  //track->setSelected(true);
-                  //mixerDetails->setTrack(mti);
-                  }
+                  for (int i = 0; i < instr->channel().size(); ++i) {
+                        Channel* chan = instr->playbackChannel(i, _score->masterScore());
 
-            if (expanded) {
-                  //Add per channel tracks
-                  const InstrumentList* il1 = part->instruments();
-                  for (auto it = il1->begin(); it != il1->end(); ++it) {
-                        Instrument* instr = it->second;
-                        for (int i = 0; i < instr->channel().size(); ++i) {
-                              Channel* chan = instr->playbackChannel(i, _score->masterScore());
+                        QTreeWidgetItem* child = new QTreeWidgetItem((QTreeWidget*)0, QStringList(chan->name()));
+                        item->addChild(child);
+                        mixerTreeWidget->setItemWidget(child, 0, mixerRowWidget(chan->name()));
 
-                              //instrumentList->addItem(chan->name());
-//                              MixerTrackItemPtr mti1 = std::make_shared<MixerTrackItem>(MixerTrackItem::TrackType::CHANNEL, part, instr, chan);
-//                              MixerTrackItemPtr mti = new MixerTrackItem(MixerTrackItem::TrackType::CHANNEL, part, instr, chan);
-//                              MixerTrackChannel* track1 = new MixerTrackChannel(this, mti1);
-//                              track1->setGroup(this);
-//                              trackList.append(track1);
-                              // holderLayout->addWidget(track1);
-
-//                              if (selPart == part && selChan == chan) {
-//                                    track1->setSelected(true);
-//                                    mixerDetails->setTrack(mti1);
-//                                    }
-                              }
                         }
                   }
+
+
             }
 
       //holderLayout->addSpacerItem(new QSpacerItem(1, 1, QSizePolicy::Expanding, QSizePolicy::Fixed));
